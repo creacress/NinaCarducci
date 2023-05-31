99960ac5d3ca2ff7ef1355df67629b3fa4534773
Cresson Alexis <alexis@webcresson.com>
2023-05-31 09:54:37 +0200

    Transform du script

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,23 @@
-$(document).ready(function () {
-  $(".gallery").mauGallery({
-    columns: {
-      xs: 1,
-      sm: 2,
-      md: 3,
-      lg: 3,
-      xl: 3,
-    },
-    lightBox: true,
-    lightboxId: "myAwesomeLightbox",
-    showTags: true,
-    tagsPosition: "top",
+// Attendre que le document HTML soit entièrement chargé
+document.addEventListener("DOMContentLoaded", function () {
+  // Sélectionner tous les éléments avec la classe "gallery"
+  var galleryElements = document.querySelectorAll(".gallery");
+
+  // Parcourir chaque élément de la galerie
+  galleryElements.forEach(function (element) {
+    // Appliquer la fonction mauGallery sur chaque élément de la galerie avec les options spécifiées
+    mauGallery(element, {
+      columns: {
+        xs: 1,  // Nombre de colonnes pour les écrans extra petits (<= 576px)
+        sm: 2,  // Nombre de colonnes pour les écrans petits (>= 576px)
+        md: 3,  // Nombre de colonnes pour les écrans moyens (>= 768px)
+        lg: 3,  // Nombre de colonnes pour les écrans larges (>= 992px)
+        xl: 3,  // Nombre de colonnes pour les écrans extra larges (>= 1200px)
+      },
+      lightBox: true,       // Activer la fonction lightbox
+      lightboxId: "myAwesomeLightbox",  // ID de la lightbox
+      showTags: true,       // Afficher les tags
+      tagsPosition: "top",  // Position des tags (en haut)
+    });
   });
 });
